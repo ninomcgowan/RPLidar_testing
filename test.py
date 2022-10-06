7c6a7dd4dc1ac63636b8236d0ade1e8273c6f80b
ninomcgowan <mcgowanj@miamioh.edu>
2022-10-06 13:27:00 -0400

    Update test.py

--- test.py
+++ test.py
@@ -12,17 +12,18 @@ lidar = RPLidar(None,PORT_NAME)
 #define variables
 
 data = [0]*360
-
+#displays health of the lidar
 print(lidar.health)
 time.sleep(5)
 
+#will try to print out the angle value, quality, and distance 
 try:
     for scan in lidar.iter_scans():
         for (quality,angle,distance) in scan:
             data[min(359,floor(angle))] = distance
             print("Angle: " + angle)
-            print(quality + data[min(359,floor(angle))] + "\n")
+            print(quality +" " data[min(359,floor(angle))] + "\n")
 except KeyboardInterrupt:
     print("Stopped")
-lidar.stop();
-lidar.disconnect();
+lidar.stop()
+lidar.disconnect()
